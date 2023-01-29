b4e95543476229e6d11c05fd6868850674e6a3c5
Victor Evogor <victorevogor0001@gmail.com>
2023-01-29 11:25:28 +0100

    Added attribution

--- src/components/App.js
+++ src/components/App.js
@@ -11,11 +11,19 @@ function App(){
     window.setMenu=setMenu;
     window.activeMenu=activeMenu;
     return (
+        <>
+
+        <div id="attribution">
+        <p>This project was made by <a href="https://linktr.ee/victorevogor" target="_blank" rel="noreferrer">Victor Evogor</a></p>
+        </div>
+        
         <div id="background">
             <div className="container">
                 {activeMenu}
             </div>
-        </div>);
+        </div>
+        </>
+    )
 }
 
 export default App;
